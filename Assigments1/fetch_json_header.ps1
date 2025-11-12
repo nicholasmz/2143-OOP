6740d7050eb2b9de16273850e50516ec4f1c2916
nicholasmz <157034912+nicholasmz@users.noreply.github.com>
2025-11-12 18:40:04 -0500

    Add files via upload

---
+++ Assigments1/fetch_json_header.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp -OutFile includes/json.hpp
+Write-Host "json.hpp downloaded successfully."
---
+++ Assigments1/fetch_json_header.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p includes
+curl -L https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp -o includes/json.hpp
+echo "json.hpp downloaded successfully."
