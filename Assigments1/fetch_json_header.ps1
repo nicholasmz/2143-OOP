92b4c04c2de667815361f8ea0021f47ab22bdd89
nicholasmz <157034912+nicholasmz@users.noreply.github.com>
2025-11-20 23:25:37 -0600

    Delete Assigments1 directory

--- Assigments1/fetch_json_header.ps1
+++
@@ -1,2 +0,0 @@
-Invoke-WebRequest https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp -OutFile includes/json.hpp
-Write-Host "json.hpp downloaded successfully."
--- Assigments1/fetch_json_header.sh
+++
@@ -1,4 +0,0 @@
-  #!/bin/bash
-mkdir -p includes
-curl -L https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp -o includes/json.hpp
-echo "json.hpp downloaded successfully."
